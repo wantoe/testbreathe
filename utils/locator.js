288c4c1dd63d891027f6085bed12dba867c5fb66
Rodger Gu <RodgerG@hotmail.co.nz>
2019-03-23 17:28:49 +1300

    started implementing test cases for the rest of the pages

--- utils/locator.js
+++ utils/locator.js
@@ -9,5 +9,21 @@ module.exports = {
     resultsPageTitle: '//*[@id="title"]',
     invalidCredentials: '/html/body/div[1]/div/div[2]/h3',
     wrongUsernamePassword: '/html/body/div[1]/div/div[2]/h3',
-    homePageAboutInfoXPath: '//*[@id="About Information"]'
+    homePageAboutInfoXPath: '//*[@id="About Information"]',
+
+    mainPageActivityXPath: '//*[@id="menu"]/li[2]/a',
+    mainPageSubmitDataXPath: '//*[@id="menu"]/li[3]/a',
+    mainPageParentalSignupXPath: '//*[@id="menu"]/li[4]/a',
+    mainPageLogoutXPath: '//*[@id="menu"]/li[5]/a',
+    mainPageGraphXPath: '//*[@id="myChart"]',
+    mainPageSubmitDateXPath: '//*[@id="input-name"]',
+    mainPageSubmitDurationXPath: '//*[@id="input-email"]',
+    mainPageSubmitSuccessfulXPath: '//*[@id="input-subject"]',
+    mainPageSubmitUnsuccessfulTimeXPath: '//*[@id="input-subject"]',
+    mainPageSubmitUnsuccessfulPressureXPath: '//*[@id="input-subject"]',
+    mainPageSubmitAveragePressureXPath: '//*[@id="input-subject"]',
+    mainPageSubmitAverageExhalationXPath: '//*[@id="input-subject"]',
+    mainPageSubmitSuccessfulCoughsXPath: '//*[@id="input-subject"]',
+    mainPageSubmitButtonXPath: '/html/body/div/div/div[2]/div[2]/div/form/div/button'
+
 }
